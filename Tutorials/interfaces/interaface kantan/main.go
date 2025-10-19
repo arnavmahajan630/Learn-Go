2bd20a117dd8db0992826bac0f329e1e84cef1d9
Ocean Whisperer <arnavmahajan630@gmail.com>
2025-10-19 12:16:22 +0530

    feat/love-interdaces

--- Tutorials/interfaces/interaface kantan/main.go
+++ Tutorials/interfaces/interaface kantan/main.go
@@ -1,5 +1,7 @@
 package main
-
+/*
+Interfaces are Soooo Cooool!!!!!
+*/
 
 // this is the contract that the db must confirm to "plug in"
 type dbcontract interface {
@@ -21,4 +23,5 @@ func NewApp(db dbcontract) * Application{
 func main() {
 	// db, err := mysql.NewConfig().DBName would return instance of mysql
 	// the db must implement all the same methods defined in the interface
+	// we can package the db with our application now we can call on db fucntions so fluently
 }
