63843fdd1412898d8944811ebe2b481253611133
Ocean Whisperer <arnavmahajan630@gmail.com>
2025-10-19 12:12:18 +0530

    feat/intro-to-interfaces

---
+++ Tutorials/interfaces/interaface kantan/go.mod
@@ -0,0 +1,3 @@
+module github.com/arnavmahajan630/learn-go/tutorials/interfaces/interface-kantan
+
+go 1.24.2
---
+++ Tutorials/interfaces/interaface kantan/main.go
@@ -0,0 +1,24 @@
+package main
+
+
+// this is the contract that the db must confirm to "plug in"
+type dbcontract interface {
+	Close()
+	InsertUser(uname string)error
+	SelectSingeUser(uname string)(string, error)
+}
+
+type Application struct {
+	db dbcontract // db is not any specifc like pg,mongo but interface
+
+}
+
+
+func NewApp(db dbcontract) * Application{
+	return &Application{db:db}
+}
+
+func main() {
+	// db, err := mysql.NewConfig().DBName would return instance of mysql
+	// the db must implement all the same methods defined in the interface
+}
