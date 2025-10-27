42a1c8db1d0ae0ddf51419d74b8d4c9c9e20f3f6
Ocean Whisperer <arnavmahajan630@gmail.com>
2025-10-27 17:18:03 +0530

    feat/tut-by-hitesh

---
+++ Tutorials/concurrency/hitesh/intro/intro.go
@@ -0,0 +1,7 @@
+package intro
+
+import "fmt"
+
+func Introduction() {
+	fmt.Println("hello and testing")
+}
---
+++ Tutorials/concurrency/hitesh/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/arnavmahajan630/learn-go/tutorials/concurrency/hitesh/intro"
+
+func main() {
+	// callers
+	intro.Introduction()
+}
