2a041306e52033ae88efffc481ba35b878f703ed
Ocean Whisperer <arnavmahajan630@gmail.com>
2025-10-27 17:22:52 +0530

    feat/quote

--- Tutorials/concurrency/hitesh/main.go
+++ Tutorials/concurrency/hitesh/main.go
@@ -2,6 +2,10 @@ package main
 
 import "github.com/arnavmahajan630/learn-go/tutorials/concurrency/hitesh/intro"
 
+
+/*
+	DO NOT COMMUNICATE BY SHARING MEMORY. INSTEAD SHARE MEMORY BY COMMUNICATING
+*/
 func main() {
 	// callers
 	intro.Introduction()
