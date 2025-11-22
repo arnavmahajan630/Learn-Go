d840fb3d00a3a58aa97f284591d6f787e473d68e
Ocean Whisperer <arnavmahajan630@gmail.com>
2025-11-22 13:11:54 +0530

    feat/Back-from-broing-semEnd-exams

--- Intermediate Projects/MyOwnDB/api/controller.go
+++ Intermediate Projects/MyOwnDB/api/controller.go
@@ -3,5 +3,5 @@ package api
 import "net/http"
 
 func Welcome(w http.ResponseWriter,  r * http.Request) {
-	w.Write([]byte("Welcome to this simple DataBase. hit /Add to add entries"))
+	w.Write([]byte("Welcome to this simple DataBase. hit Post on '/' Add to add entries"))
 }
--- Intermediate Projects/MyOwnDB/go.mod
+++ Intermediate Projects/MyOwnDB/go.mod
@@ -2,4 +2,4 @@ module github.com/arnavmahajan630/Learn-Go/Intermediate-Projects/MyOwnDb
 
 go 1.24.2
 
-require github.com/go-chi/chi/v5 v5.2.3 // indirect
+require github.com/go-chi/chi/v5 v5.2.3 // direct
