205c74ad3d0157279ef3e4bb2c958d56a1d20725
arnavmahajan630@gmail.com <arnavmahajan630@gmail.com>
2025-12-07 21:06:31 +0530

    feat/routes-setup-complete

--- Intermediate Projects/JWT Golang/routes/userRoutes.go
+++ Intermediate Projects/JWT Golang/routes/userRoutes.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	"github.com/arnavmahajan630/Learn-Go/Intermediate-Projects/JWT-Golang/controllers"
+	"github.com/gin-gonic/gin"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func userRoutes(ir * gin.Engine) {
+	ir.Use(middleware.Authenticate())
+	ir.GET("/users", controllers.getUsers)
+	ir.GET("/user/:userid", controllers.getUser)
+}
