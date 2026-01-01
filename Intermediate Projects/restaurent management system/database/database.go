f8caa5b6c6ab0e5f92916757fe6c38d475b0e972
arnavmahajan630@gmail.com <arnavmahajan630@gmail.com>
2026-01-01 16:18:06 +0530

    feat/log-db

--- Intermediate Projects/restaurent management system/database/database.go
+++ Intermediate Projects/restaurent management system/database/database.go
@@ -1,1 +1,37 @@
 package database
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func main() {
+  serverAPI := options.ServerAPI(options.ServerAPIVersion1)
+  dbUrl := os.Getenv("DATABASE_URL")
+  if(dbUrl == "") {
+	log.Fatal("Error Fetching The Database URL")
+  }
+  opts := options.Client().ApplyURI(dbUrl).SetServerAPIOptions(serverAPI)
+
+  client, err := mongo.Connect(context.Background(), opts)
+  if err != nil {
+    panic(err)
+  }
+
+  defer func() {
+    if err = client.Disconnect(context.TODO()); err != nil {
+      panic(err)
+    }
+  }()
+
+  // Send a ping to confirm a successful connection
+  if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
+    panic(err)
+  }
+  log.Println("Pinged the Deployment. Successfully connected to MongoDB!")
+}
