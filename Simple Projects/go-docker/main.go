7fdf10a16307aa0c8374621c6bedc8e32f354f65
arnavmahajan630@gmail.com <arnavmahajan630@gmail.com>
2026-01-03 19:29:39 +0530

    feat/setup-routes

--- Simple Projects/go-docker/main.go
+++ Simple Projects/go-docker/main.go
@@ -1,8 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html"
+	"net/http"
+)
 
+func handleRoot(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Root of the Project :) %q", html.EscapeString(r.URL.Path))
+}
+func handleHi(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the Hi of the Project :) %q", html.EscapeString(r.URL.Path))
+}
 func main() {
 
 	fmt.Println("hello to docker :) ")
+
+	http.HandleFunc("/", handleRoot)
+	http.HandleFunc("/hi", handleHi)
 }
