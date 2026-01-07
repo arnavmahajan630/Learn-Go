bf963d8df7f24ed1dd1720baf5fc6fbdcad7d9ab
arnavmahajan630@gmail.com <arnavmahajan630@gmail.com>
2026-01-07 18:29:58 +0530

    feat/changes-to-env-switched-project-to-chi

--- Simple Projects/golang-oauth/go.mod
+++ Simple Projects/golang-oauth/go.mod
@@ -3,3 +3,5 @@ module github.com/arnavmahajan630/Learn-Go/Simple-Proejcts/golang-oauth
 go 1.25.5
 
 require github.com/joho/godotenv v1.5.1
+
+require github.com/go-chi/chi/v5 v5.2.3  // direct
--- Simple Projects/golang-oauth/main.go
+++ Simple Projects/golang-oauth/main.go
@@ -6,23 +6,20 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	_ "github.com/joho/godotenv/autoload"
 )
 
-func setupRoutes(m *http.ServeMux) {
-	m.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Welcome to home point")
-	})
-}
 
 func main() {
 	port := os.Getenv("PORT")
-	if port == "" {
-		log.Fatal("PORT not set")
+	if port == ""{
+		port = ":3000"
 	}
-
-	m := http.NewServeMux()
-	setupRoutes(m)
-
-	log.Fatal(http.ListenAndServe(port, nil))
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {w.Write([]byte("hello to root endpoint"))})
+	r.Get("/home", func(w http.ResponseWriter, r *http.Request) {fmt.Fprintf(w, "Welcome to home endpoint")})
+	log.Fatal(http.ListenAndServe(port, r))
 }
