040d37296891efad2fb67d4860ad265d10b49f17
arnavmahajan630@gmail.com <arnavmahajan630@gmail.com>
2026-01-14 18:52:45 +0530

    feat/simple-endpoint

---
+++ Intermediate Projects/rate-limiter/token-bucket/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Message struct {
+	Satus string `json:"status"`
+	Body string `json:"body"`
+}
+
+func endPointHander(w http.ResponseWriter, r * http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	message := Message {
+		Satus: "Successful",
+		Body: "Hi! Reached the endpoint :)",
+	}
+	if err := json.NewEncoder(w).Encode(&message); err != nil {
+		return
+	}
+}
+
+func main() {
+
+	http.HandleFunc("/endpoint", endPointHander)
+	fmt.Println("Server started on Port 3000 :| ")
+	log.Fatal(http.ListenAndServe(":3000", nil))
+
+}
