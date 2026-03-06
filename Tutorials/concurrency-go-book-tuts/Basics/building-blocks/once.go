abab5e9147de5fd4dbfafa19e631df978a50315a
arnavmahajan630@gmail.com <arnavmahajan630@gmail.com>
2026-03-06 12:58:52 +0530

    pool setup

--- Tutorials/concurrency-go-book-tuts/Basics/building-blocks/once.go
+++ Tutorials/concurrency-go-book-tuts/Basics/building-blocks/once.go
@@ -1,35 +1,35 @@
 package main
 
-import (
-	"fmt"
-	"sync"
-)
+// import (
+// 	"fmt"
+// 	"sync"
+// )
 
 
-func main() {
+// func main() {
 	
-	var count int = 0
+// 	var count int = 0
 
-	increment := func ()  {
-		count++
-	}
-	decrement := func ()  {
-		count--
-	}
-	var once sync.Once
-	var twice sync.Once
-	var wg sync.WaitGroup
-	wg.Add(20)
-	for i := 0; i < 20; i++ {
-		go func ()  {
-			defer wg.Done()
-		once.Do(increment)
-		// once.Do(decrement) 
-		twice.Do(decrement)
-		}()
-	}
+// 	increment := func ()  {
+// 		count++
+// 	}
+// 	decrement := func ()  {
+// 		count--
+// 	}
+// 	var once sync.Once
+// 	var twice sync.Once
+// 	var wg sync.WaitGroup
+// 	wg.Add(20)
+// 	for i := 0; i < 20; i++ {
+// 		go func ()  {
+// 			defer wg.Done()
+// 		once.Do(increment)
+// 		// once.Do(decrement) 
+// 		twice.Do(decrement)
+// 		}()
+// 	}
 
-	wg.Wait()
-	fmt.Printf("Count is: %d\n", count)
+// 	wg.Wait()
+// 	fmt.Printf("Count is: %d\n", count)
 
-}
+// }
---
+++ Tutorials/concurrency-go-book-tuts/Basics/building-blocks/pool.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello to Pools in Go! \n\n")
+}
