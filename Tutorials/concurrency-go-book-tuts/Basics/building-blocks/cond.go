b6fceba4c2c9c6e2ae23b610043fc8c17df52fe6
arnavmahajan630@gmail.com <arnavmahajan630@gmail.com>
2026-03-06 12:53:50 +0530

    package once disection completed.

--- Tutorials/concurrency-go-book-tuts/Basics/building-blocks/cond.go
+++ Tutorials/concurrency-go-book-tuts/Basics/building-blocks/cond.go
@@ -1,46 +1,46 @@
-package main
+ package main
 
-import (
-	"fmt"
-	"sync"
-)
+// import (
+// 	"fmt"
+// 	"sync"
+// )
 
-var (
-	mu    sync.Mutex
-	cond  = sync.NewCond(&mu)
-	tasks []string
-	wg sync.WaitGroup
-)
+// var (
+// 	mu    sync.Mutex
+// 	cond  = sync.NewCond(&mu)
+// 	tasks []string
+// 	wg sync.WaitGroup
+// )
 
-func worker(id int) {
-	defer wg.Done()
-	cond.L.Lock()
+// func worker(id int) {
+// 	defer wg.Done()
+// 	cond.L.Lock()
 
-	for len(tasks) == 0 {
-		fmt.Println("Worker", id, "waiting for task")
-		cond.Wait()
-	}
+// 	for len(tasks) == 0 {
+// 		fmt.Println("Worker", id, "waiting for task")
+// 		cond.Wait()
+// 	}
 
-	task := tasks[0]
-	tasks = tasks[1:]
+// 	task := tasks[0]
+// 	tasks = tasks[1:]
 
-	cond.L.Unlock()
+// 	cond.L.Unlock()
 
-	fmt.Println("Worker", id, "processing:", task)
-}
+// 	fmt.Println("Worker", id, "processing:", task)
+// }
 
-func main() {
+// func main() {
 
-	go worker(1)
+// 	go worker(1)
 
-	wg.Add(1)
-	cond.L.Lock()
-	tasks = append(tasks, "send email")
-	fmt.Println("Task added")
-	cond.Signal()
-	cond.L.Unlock()
+// 	wg.Add(1)
+// 	cond.L.Lock()
+// 	tasks = append(tasks, "send email")
+// 	fmt.Println("Task added")
+// 	cond.Signal()
+// 	cond.L.Unlock()
 
-	wg.Wait()
+// 	wg.Wait()
 
-	fmt.Println("Task processed successfully")
-}
+// 	fmt.Println("Task processed successfully")
+// }
---
+++ Tutorials/concurrency-go-book-tuts/Basics/building-blocks/once.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+
+func main() {
+	
+	var count int = 0
+
+	increment := func ()  {
+		count++
+	}
+	decrement := func ()  {
+		count--
+	}
+	var once sync.Once
+	var twice sync.Once
+	var wg sync.WaitGroup
+	wg.Add(20)
+	for i := 0; i < 20; i++ {
+		go func ()  {
+			defer wg.Done()
+		once.Do(increment)
+		// once.Do(decrement) 
+		twice.Do(decrement)
+		}()
+	}
+
+	wg.Wait()
+	fmt.Printf("Count is: %d\n", count)
+
+}
