76e093d3ae07f9739d7adbb44907be82a49704ff
arnavmahajan630@gmail.com <arnavmahajan630@gmail.com>
2026-03-11 23:21:49 +0530

    channels setup

--- Tutorials/concurrency-go-book-tuts/Basics/building-blocks/pool.go
+++ Tutorials/concurrency-go-book-tuts/Basics/building-blocks/pool.go
@@ -1,21 +1,21 @@
-package main
+ package main
 
-import (
-    "fmt"
-    "sync"
-)
+// import (
+//     "fmt"
+//     "sync"
+// )
 
-var pool = sync.Pool{
-    New: func() any {
-        fmt.Println("Allocating new object")
-        return make([]byte, 1024)
-    },
-}
+// var pool = sync.Pool{
+//     New: func() any {
+//         fmt.Println("Allocating new object")
+//         return make([]byte, 1024)
+//     },
+// }
 
-func main() {
-    b1 := pool.Get().([]byte)
-    pool.Put(b1)
+// func main() {
+//     b1 := pool.Get().([]byte)
+//     pool.Put(b1)
 
-    b2 := pool.Get().([]byte)
-    pool.Put(b2)
-}
+//     b2 := pool.Get().([]byte)
+//     pool.Put(b2)
+// }
