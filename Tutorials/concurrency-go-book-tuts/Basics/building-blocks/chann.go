41dfcc1855aa14951a1f85c22f9855eb6afd6365
arnavmahajan630@gmail.com <arnavmahajan630@gmail.com>
2026-05-15 11:02:38 +0530

    more on channels. and channel notes.

--- Tutorials/concurrency-go-book-tuts/Basics/building-blocks/chann.go
+++ Tutorials/concurrency-go-book-tuts/Basics/building-blocks/chann.go
@@ -1,32 +1,32 @@
 	package main
 
-	import (
-		"fmt"
-		"sync"
-	)
+	// import (
+	// 	"fmt"
+	// 	"sync"
+	// )
 
-	func consumer(ch <- chan int,  wg * sync.WaitGroup) {
-		defer wg.Done()
-		for i := range ch {
-			fmt.Println(i)
-		}
-	}
+	// func consumer(ch <- chan int,  wg * sync.WaitGroup) {
+	// 	defer wg.Done()
+	// 	for i := range ch {
+	// 		fmt.Println(i)
+	// 	}
+	// }
 
-	func producer(ch chan <- int, wg * sync.WaitGroup) {
-		defer wg.Done()
-		defer close(ch)
-		for i := 1; i <= 5; i++ {
-			ch <- i
-		}
-	}
+	// func producer(ch chan <- int, wg * sync.WaitGroup) {
+	// 	defer wg.Done()
+	// 	defer close(ch)
+	// 	for i := 1; i <= 5; i++ {
+	// 		ch <- i
+	// 	}
+	// }
 
-	func main() {
+	// func main() {
 
-	var wg sync.WaitGroup
-		wg.Add(2)
-		ch := make(chan int)
-		go producer(ch, &wg)
-		go consumer(ch, &wg)
-		wg.Wait()
-		fmt.Println("Exchanged data successfully")
-	}
+	// var wg sync.WaitGroup
+	// 	wg.Add(2)
+	// 	ch := make(chan int)
+	// 	go producer(ch, &wg)
+	// 	go consumer(ch, &wg)
+	// 	wg.Wait()
+	// 	fmt.Println("Exchanged data successfully")
+	// }
