a20d56ad8eccff02ca43bda3953330f155d5d923
arnavmahajan630@gmail.com <arnavmahajan630@gmail.com>
2026-05-19 11:27:23 +0530

    comment out select block tut

--- Tutorials/concurrency-go-book-tuts/Basics/building-blocks/select.go
+++ Tutorials/concurrency-go-book-tuts/Basics/building-blocks/select.go
@@ -1,22 +1,22 @@
 package main
 
-import (
-	"fmt"
-	"time"
-)
+// import (
+// 	"fmt"
+// 	"time"
+// )
 
-func main() {
-	startTime := time.Now()
-	ch := make(chan any)
+// func main() {
+// 	startTime := time.Now()
+// 	ch := make(chan any)
 
-	go func() {
-		time.Sleep(5* time.Second)
-		close(ch)
-	}()
+// 	go func() {
+// 		time.Sleep(5* time.Second)
+// 		close(ch)
+// 	}()
 
-	fmt.Println("Blocking on Read")
-	select {
-	case <- ch:
-		fmt.Printf("Unblocking %v later\n", time.Since(startTime))
-	}
-}
+// 	fmt.Println("Blocking on Read")
+// 	select {
+// 	case <- ch:
+// 		fmt.Printf("Unblocking %v later\n", time.Since(startTime))
+// 	}
+// }
